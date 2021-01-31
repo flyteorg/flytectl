b143a5db80437dc2f63a8bb0b3b902c9a7802871
pmahindrakar <pmahindrakar@vmware.com>
2021-01-31 17:01:21 +0530

    Fixed unit test

--- cmd/update/update_test.go
+++ cmd/update/update_test.go
@@ -10,14 +10,12 @@ func TestUpdateCommand(t *testing.T) {
 	updateCommand := CreateUpdateCommand()
 	assert.Equal(t, updateCommand.Use , "update")
 	assert.Equal(t, updateCommand.Short , "Update various resources.")
-	assert.Equal(t, len(updateCommand.Commands()), 2)
+	assert.Equal(t, len(updateCommand.Commands()), 1)
 	cmdNouns := updateCommand.Commands()
 	// Sort by Use value.
 	sort.Slice(cmdNouns, func(i, j int) bool {
 		return cmdNouns[i].Use < cmdNouns[j].Use
 	})
-	assert.Equal(t, cmdNouns[0].Use, "activate-project")
-	assert.Equal(t, cmdNouns[0].Aliases, []string{"activate"})
-	assert.Equal(t, cmdNouns[1].Use, "archive-project")
-	assert.Equal(t, cmdNouns[1].Aliases, []string{"archive"})
+	assert.Equal(t, cmdNouns[0].Use, "project")
+	assert.Equal(t, cmdNouns[0].Aliases, []string{"projects"})
 }
