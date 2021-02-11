0a30444f2f416704c98aa865e7561978b049b39d
pmahindrakar <pmahindrakar@vmware.com>
2021-02-11 22:14:18 +0530

    Moved the constants to there respective files

--- cmd/register/register.go
+++ cmd/register/register.go
@@ -14,39 +14,6 @@ Currently these input files are protobuf files generated as output from flytekit
 Project & Domain are mandatory fields to be passed for registration and an optional version which defaults to v1
 If the entities are already registered with flyte for the same version then registration would fail.
 `
-	registerFilesShort = "Registers file resources"
-	registerFilesLong  = `
-Registers all the serialized protobuf files including tasks, workflows and launchplans with default v1 version.
-If there are already registered entities with v1 version then the command will fail immediately on the first such encounter.
-::
-
- bin/flytectl register file  _pb_output/* -d development  -p flytesnacks
-
-If you want to continue executing registration on other files ignoring the errors including version conflicts then pass in
-the skipOnError flag.
-
-::
-
- bin/flytectl register file  _pb_output/* -d development  -p flytesnacks --skipOnError
-
-Using short format of skipOnError flag
-::
-
- bin/flytectl register file  _pb_output/* -d development  -p flytesnacks -s
-
-Overriding the default version v1 using version string.
-::
-
- bin/flytectl register file  _pb_output/* -d development  -p flytesnacks -v v2
-
-Change the o/p format has not effect on registration. The O/p is currently available only in table format.
-
-::
-
- bin/flytectl register file  _pb_output/* -d development  -p flytesnacks -s -o yaml
-
-Usage
-`
 )
 
 // RegisterCommand will return register command
--- cmd/update/update.go
+++ cmd/update/update.go
@@ -20,51 +20,6 @@ Example update project to activate it.
 
  bin/flytectl update project -p flytesnacks --activateProject
 `
-	projectShort = "Updates project resources"
-	projectLong  = `
-Updates the project according the flags passed.Allows you to archive or activate a project.
-Activates project named flytesnacks.
-::
-
- bin/flytectl update project -p flytesnacks --activateProject
-
-Archives project named flytesnacks.
-
-::
-
- bin/flytectl get project flytesnacks --archiveProject
-
-Activates project named flytesnacks using short option -t.
-::
-
- bin/flytectl update project -p flytesnacks -t
-
-Archives project named flytesnacks using short option -a.
-
-::
-
- bin/flytectl update project flytesnacks -a
-
-Incorrect usage when passing both archive and activate.
-
-::
-
- bin/flytectl update project flytesnacks -a -t
-
-Incorrect usage when passing unknown-project.
-
-::
-
- bin/flytectl update project unknown-project -a
-
-Incorrect usage when passing valid project using -p option.
-
-::
-
- bin/flytectl update project unknown-project -a -p known-project
-
-Usage
-`
 )
 
 // CreateUpdateCommand will return update command
