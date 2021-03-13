89b585e2f3605972d2b17d1d0fe95aa983104379
brucearctor <5032356+brucearctor@users.noreply.github.com>
2021-03-13 11:56:52 -0800

    revert boilerplate

--- boilerplate/lyft/golang_support_tools/tools.go
+++ boilerplate/lyft/golang_support_tools/tools.go
@@ -5,6 +5,6 @@ package tools
 import (
 	_ "github.com/alvaroloes/enumer"
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
-	_ "github.com/flyte/flytestdlib/cli/pflags"
+	_ "github.com/lyft/flytestdlib/cli/pflags"
 	_ "github.com/vektra/mockery/cmd/mockery"
 )
