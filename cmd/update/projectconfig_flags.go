c4d7da622959a6086fce1781c6abaf183cc8759a
Prafulla Mahindrakar <prafulla.mahindrakar@gmail.com>
2021-05-13 12:39:54 +0530

    Removed the generated code in flag files which is not used and reducing ut coverage
    Signed-off-by: Prafulla Mahindrakar <prafulla.mahindrakar@gmail.com>

--- cmd/update/projectconfig_flags.go
+++ cmd/update/projectconfig_flags.go
@@ -4,38 +4,10 @@
 package update
 
 import (
-	"encoding/json"
-	"reflect"
-
 	"fmt"
 
 	"github.com/spf13/pflag"
 )
-
-// If v is a pointer, it will get its element value or the zero value of the element type.
-// If v is not a pointer, it will return it as is.
-func (ProjectConfig) elemValueOrNil(v interface{}) interface{} {
-	if t := reflect.TypeOf(v); t.Kind() == reflect.Ptr {
-		if reflect.ValueOf(v).IsNil() {
-			return reflect.Zero(t.Elem()).Interface()
-		} else {
-			return reflect.ValueOf(v).Interface()
-		}
-	} else if v == nil {
-		return reflect.Zero(t).Interface()
-	}
-
-	return v
-}
-
-func (ProjectConfig) mustMarshalJSON(v json.Marshaler) string {
-	raw, err := v.MarshalJSON()
-	if err != nil {
-		panic(err)
-	}
-
-	return string(raw)
-}
 
 // GetPFlagSet will return strongly types pflags for all fields in ProjectConfig and its nested types. The format of the
 // flags is json-name.json-sub-name... etc.
