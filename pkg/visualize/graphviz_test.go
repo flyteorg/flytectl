d420faf8f95b45ba12d7af5fa9c1e174a0435979
Ketan Umare <ketan.umare@gmail.com>
2021-06-01 16:33:15 -0700

    Working with subworkflows
    Signed-off-by: Ketan Umare <ketan.umare@gmail.com>
    Signed-off-by: Prafulla Mahindrakar <prafulla.mahindrakar@gmail.com>

--- pkg/visualize/graphviz_test.go
+++ pkg/visualize/graphviz_test.go
@@ -12,8 +12,23 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestRenderWorkflow(t *testing.T) {
+func TestRenderWorkflowBranch(t *testing.T) {
 	r, err := ioutil.ReadFile("testdata/compiled_closure_branch_nested.json")
+	assert.NoError(t, err)
+
+	i := bytes.NewReader(r)
+
+	c := &core.CompiledWorkflowClosure{}
+	err = jsonpb.Unmarshal(i, c)
+	assert.NoError(t, err)
+	b, err := RenderWorkflow(c, graphviz.PNG)
+	assert.NoError(t, err)
+	assert.NotNil(t, b)
+	ioutil.WriteFile("/tmp/image.png", b, fs.ModePerm)
+}
+
+func TestRenderWorkflowSubWorkflow(t *testing.T) {
+	r, err := ioutil.ReadFile("testdata/compiled_subworkflows.json")
 	assert.NoError(t, err)
 
 	i := bytes.NewReader(r)
