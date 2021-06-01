364dd9b5859b60f687f42cefaaf93280f6825192
Ketan Umare <ketan.umare@gmail.com>
2021-06-01 16:49:36 -0700

    subworkflows rendering
    Signed-off-by: Ketan Umare <ketan.umare@gmail.com>
    Signed-off-by: Prafulla Mahindrakar <prafulla.mahindrakar@gmail.com>

--- pkg/visualize/graphviz_test.go
+++ pkg/visualize/graphviz_test.go
@@ -2,10 +2,11 @@ package visualize
 
 import (
 	"bytes"
-	"github.com/goccy/go-graphviz"
-	"io/fs"
+	"fmt"
 	"io/ioutil"
 	"testing"
+
+	"github.com/goccy/go-graphviz"
 
 	"github.com/flyteorg/flyteidl/gen/pb-go/flyteidl/core"
 	"github.com/golang/protobuf/jsonpb"
@@ -13,31 +14,22 @@ import (
 )
 
 func TestRenderWorkflowBranch(t *testing.T) {
-	r, err := ioutil.ReadFile("testdata/compiled_closure_branch_nested.json")
-	assert.NoError(t, err)
+	// Sadly we cannot compare the output of svg, as it slightly changes.
+	file := []string{"compiled_closure_branch_nested", "compiled_subworkflows"}
 
-	i := bytes.NewReader(r)
+	for _, s := range file {
+		t.Run(s, func(t *testing.T) {
+			r, err := ioutil.ReadFile(fmt.Sprintf("testdata/%s.json", s))
+			assert.NoError(t, err)
 
-	c := &core.CompiledWorkflowClosure{}
-	err = jsonpb.Unmarshal(i, c)
-	assert.NoError(t, err)
-	b, err := RenderWorkflow(c, graphviz.PNG)
-	assert.NoError(t, err)
-	assert.NotNil(t, b)
-	ioutil.WriteFile("/tmp/image.png", b, fs.ModePerm)
-}
+			i := bytes.NewReader(r)
 
-func TestRenderWorkflowSubWorkflow(t *testing.T) {
-	r, err := ioutil.ReadFile("testdata/compiled_subworkflows.json")
-	assert.NoError(t, err)
-
-	i := bytes.NewReader(r)
-
-	c := &core.CompiledWorkflowClosure{}
-	err = jsonpb.Unmarshal(i, c)
-	assert.NoError(t, err)
-	b, err := RenderWorkflow(c, graphviz.PNG)
-	assert.NoError(t, err)
-	assert.NotNil(t, b)
-	ioutil.WriteFile("/tmp/image.png", b, fs.ModePerm)
+			c := &core.CompiledWorkflowClosure{}
+			err = jsonpb.Unmarshal(i, c)
+			assert.NoError(t, err)
+			b, err := RenderWorkflow(c, graphviz.SVG)
+			assert.NoError(t, err)
+			assert.NotNil(t, b)
+		})
+	}
 }
