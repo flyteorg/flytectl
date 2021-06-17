60fb3557ec210898cfe4b6370ac95d0238244e9f
pmahindrakar-oss <77798312+pmahindrakar-oss@users.noreply.github.com>
2021-06-17 09:19:53 +0530

    Using go graphviz library to print dot file (#97)
    * Using go graphviz library to print dot file
    
    Signed-off-by: Prafulla Mahindrakar <prafulla.mahindrakar@gmail.com>
    
    * Rebased
    
    Signed-off-by: Prafulla Mahindrakar <prafulla.mahindrakar@gmail.com>

--- cmd/config/subcommand/workflow/workflow_config.go
+++ cmd/config/subcommand/workflow/workflow_config.go
@@ -1,9 +1,7 @@
 package workflow
 
 import (
-	"fmt"
 	"github.com/flyteorg/flytectl/pkg/filters"
-	"github.com/goccy/go-graphviz"
 )
 
 //go:generate pflags Config --default-var DefaultConfig
@@ -21,19 +19,4 @@ type Config struct {
 	Filter  filters.Filters `json:"filter" pflag:","`
 	Visualize  string `json:"visualize" pflag:",optional flag to visualize a workflow as one of [png, dot, svg, jpg]"`
 	OutputFile string `json:"output_file" pflag:",path and a filename of where the output image should be dumped. This can only be used in concert with visualize"`
-}
-
-// GraphvizFormat returns the graphviz format based on the user-specified value or an error
-func (cfg *Config) GraphvizFormat() (graphviz.Format, error) {
-	switch strings.ToLower(cfg.Visualize) {
-	case "png":
-		return graphviz.PNG, nil
-	case "svg":
-		return graphviz.SVG, nil
-	case "dot":
-		return graphviz.XDOT, nil
-	case "jpg":
-		return graphviz.JPG, nil
-	}
-	return graphviz.SVG, fmt.Errorf("unsupported visualization format [%s]. Supported [png, dot, svg, jpg]", cfg.Visualize)
 }
