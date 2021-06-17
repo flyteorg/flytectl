462d0d95db1ccd56ce447c07046f1c7a46143763
Haytham Abuelfutuh <haytham@afutuh.com>
2021-06-17 10:28:06 -0700

    Only pass literals to execution if they users provided values
    Signed-off-by: Haytham Abuelfutuh <haytham@afutuh.com>

--- cmd/create/serialization_utils.go
+++ cmd/create/serialization_utils.go
@@ -10,10 +10,14 @@ func MakeLiteralForVariables(serialize map[string]interface{}, variables map[str
 	result := make(map[string]*core.Literal)
 	var err error
 	for k, v := range variables {
-		if result[k], err = coreutils.MakeLiteralForType(v.Type, serialize[k]); err != nil {
-			return nil, err
+		// Only serialize inputs that are provided
+		if input, provided := serialize[k]; provided {
+			if result[k], err = coreutils.MakeLiteralForType(v.Type, input); err != nil {
+				return nil, err
+			}
 		}
 	}
+
 	return result, nil
 }
 
@@ -21,9 +25,13 @@ func MakeLiteralForParams(serialize map[string]interface{}, parameters map[strin
 	result := make(map[string]*core.Literal)
 	var err error
 	for k, v := range parameters {
-		if result[k], err = coreutils.MakeLiteralForType(v.GetVar().Type, serialize[k]); err != nil {
-			return nil, err
+		// Only serialize inputs that are provided
+		if input, provided := serialize[k]; provided {
+			if result[k], err = coreutils.MakeLiteralForType(v.GetVar().Type, input); err != nil {
+				return nil, err
+			}
 		}
 	}
+
 	return result, nil
 }
