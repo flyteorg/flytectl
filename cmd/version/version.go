479d24172663db6c0c62b553a5b7150cba4a693a
Yuvraj <code@evalsocket.dev>
2021-06-20 10:10:43 +0530

    wip: added latest version check added
    Signed-off-by: Yuvraj <code@evalsocket.dev>

--- cmd/version/version.go
+++ cmd/version/version.go
@@ -4,10 +4,14 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 
 	cmdCore "github.com/flyteorg/flytectl/cmd/core"
 	"github.com/flyteorg/flyteidl/gen/pb-go/flyteidl/admin"
+	"github.com/flyteorg/flytestdlib/logger"
 	stdlibversion "github.com/flyteorg/flytestdlib/version"
+	hversion "github.com/hashicorp/go-version"
 	"github.com/spf13/cobra"
 )
 
@@ -21,6 +25,8 @@ Example version.
  bin/flytectl version
 `
 )
+
+var githubBaseURL = "https://api.github.com"
 
 type versionOutput struct {
 	// Specifies the Name of app
@@ -44,12 +50,12 @@ func GetVersionCommand(rootCmd *cobra.Command) map[string]cmdCore.CommandEntry {
 }
 
 func getVersion(ctx context.Context, args []string, cmdCtx cmdCore.CommandContext) error {
-
-	v, err := cmdCtx.AdminClient().GetVersion(ctx, &admin.GetVersionRequest{})
+	latest, err := getLatestVersion(githubBaseURL)
 	if err != nil {
 		return fmt.Errorf("err %v: ", err)
 	}
 
+	fmt.Println(compareVersion(latest, stdlibversion.Version))
 	// Print Flytectl
 	if err := printVersion(versionOutput{
 		Build:     stdlibversion.Build,
@@ -60,15 +66,7 @@ func getVersion(ctx context.Context, args []string, cmdCtx cmdCore.CommandContex
 		return err
 	}
 
-	// Print Flyteadmin
-	if err := printVersion(versionOutput{
-		Build:     v.ControlPlaneVersion.Build,
-		BuildTime: v.ControlPlaneVersion.BuildTime,
-		Version:   v.ControlPlaneVersion.Version,
-		App:       "controlPlane",
-	}); err != nil {
-		return err
-	}
+	getControlePlaneVersion(ctx, cmdCtx)
 	return nil
 }
 
@@ -79,4 +77,51 @@ func printVersion(response versionOutput) error {
 	}
 	fmt.Print(string(b))
 	return nil
+}
+
+func compareVersion(latest, current string) string {
+	latestVersion, _ := hversion.NewVersion(latest)
+	fversion, _ := hversion.NewVersion(current)
+
+	if fversion.LessThan(latestVersion) {
+		return fmt.Sprintf("A newer version of flytectl is available [%v] Please upgrade using - https://docs.flyte.org/projects/flytectl/en/latest/index.html", latest)
+	}
+
+	return "Installed flytectl version is the latest"
+}
+
+func getControlePlaneVersion(ctx context.Context, cmdCtx cmdCore.CommandContext) {
+	v, err := cmdCtx.AdminClient().GetVersion(ctx, &admin.GetVersionRequest{})
+	if err != nil || v == nil {
+		logger.Debugf(ctx, "Failed to get version of control plane %v: \n", err)
+		return
+	}
+	// Print Flyteadmin
+	if err := printVersion(versionOutput{
+		Build:     v.ControlPlaneVersion.Build,
+		BuildTime: v.ControlPlaneVersion.BuildTime,
+		Version:   v.ControlPlaneVersion.Version,
+		App:       "controlPlane",
+	}); err != nil {
+		logger.Debugf(ctx, "Not able to get control plane version..Please try again : %v \n", err)
+	}
+}
+
+func getLatestVersion(baseURL string) (string, error) {
+	response, err := http.Get(fmt.Sprintf("%v/repos/flyteorg/flytectl/releases/latest", baseURL))
+	if err != nil {
+		return "", err
+	}
+	defer response.Body.Close()
+
+	data, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return "", err
+	}
+	var result = make(map[string]interface{})
+	err = json.Unmarshal(data, &result)
+	if err != nil {
+		return "", err
+	}
+	return result["tag_name"].(string), nil
 }
--- cmd/version/version_test.go
+++ cmd/version/version_test.go
@@ -4,31 +4,102 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"sort"
 	"testing"
+
+	"github.com/spf13/cobra"
 
 	cmdCore "github.com/flyteorg/flytectl/cmd/core"
 	"github.com/flyteorg/flyteidl/clients/go/admin/mocks"
 	"github.com/flyteorg/flyteidl/gen/pb-go/flyteidl/admin"
+	stdlibversion "github.com/flyteorg/flytestdlib/version"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestListExecutionFunc(t *testing.T) {
+var (
+	versionRequest  = &admin.GetVersionRequest{}
+	testVersion     = "v0.1.20"
+	versionResponse = &admin.GetVersionResponse{
+		ControlPlaneVersion: &admin.Version{
+			Build:     "",
+			BuildTime: "",
+			Version:   testVersion,
+		},
+	}
+)
+
+func TestVersionCommand(t *testing.T) {
+	rootCmd := &cobra.Command{
+		Long:              "flytectl is CLI tool written in go to interact with flyteadmin service",
+		Short:             "flyetcl CLI tool",
+		Use:               "flytectl",
+		DisableAutoGenTag: true,
+	}
+	versionCommand := GetVersionCommand(rootCmd)
+	cmdCore.AddCommands(rootCmd, versionCommand)
+	fmt.Println(rootCmd.Commands())
+	assert.Equal(t, len(rootCmd.Commands()), 1)
+	cmdNouns := rootCmd.Commands()
+	// Sort by Use value.
+	sort.Slice(cmdNouns, func(i, j int) bool {
+		return cmdNouns[i].Use < cmdNouns[j].Use
+	})
+
+	assert.Equal(t, cmdNouns[0].Use, "version")
+	assert.Equal(t, cmdNouns[0].Short, versionCmdShort)
+	assert.Equal(t, cmdNouns[0].Long, versionCmdLong)
+}
+
+func TestVersionCommandFunc(t *testing.T) {
 	ctx := context.Background()
 	var args []string
 	mockClient := new(mocks.AdminServiceClient)
 	mockOutStream := new(io.Writer)
 	cmdCtx := cmdCore.NewCommandContext(mockClient, *mockOutStream)
-	versionRequest := &admin.GetVersionRequest{}
-	versionResponse := &admin.GetVersionResponse{
-		ControlPlaneVersion: &admin.Version{
-			Build:     "",
-			BuildTime: "",
-			Version:   "",
-		},
-	}
+	stdlibversion.Build = ""
+	stdlibversion.BuildTime = ""
+	stdlibversion.Version = testVersion
 	mockClient.OnGetVersionMatch(ctx, versionRequest).Return(versionResponse, nil)
 	err := getVersion(ctx, args, cmdCtx)
-	fmt.Println(err)
-	assert.Nil(t, nil)
+	assert.Nil(t, err)
 	mockClient.AssertCalled(t, "GetVersion", ctx, versionRequest)
+}
+
+func TestVersionCommandFuncErr(t *testing.T) {
+	ctx := context.Background()
+	var args []string
+	mockClient := new(mocks.AdminServiceClient)
+	mockOutStream := new(io.Writer)
+	cmdCtx := cmdCore.NewCommandContext(mockClient, *mockOutStream)
+	stdlibversion.Build = ""
+	stdlibversion.BuildTime = ""
+	stdlibversion.Version = testVersion
+	githubBaseURL = "ht://github.io"
+	mockClient.OnGetVersionMatch(ctx, versionRequest).Return(versionResponse, nil)
+	err := getVersion(ctx, args, cmdCtx)
+	assert.NotNil(t, err)
+}
+
+func TestVersionUtilFunc(t *testing.T) {
+	stdlibversion.Build = ""
+	stdlibversion.BuildTime = ""
+	stdlibversion.Version = testVersion
+	t.Run("Get latest release with wrong url", func(t *testing.T) {
+		tag, err := getLatestVersion("h://api.github.com/repos/flyteorg/flytectreleases/latest")
+		assert.NotNil(t, err)
+		assert.Equal(t, len(tag), 0)
+	})
+	t.Run("Compare flytectl version", func(t *testing.T) {
+		message := compareVersion(testVersion, testVersion)
+		assert.Equal(t, "Installed flytectl version is the latest", message)
+	})
+	t.Run("Error in getting control plan version", func(t *testing.T) {
+		ctx := context.Background()
+		mockClient := new(mocks.AdminServiceClient)
+		mockOutStream := new(io.Writer)
+		cmdCtx := cmdCore.NewCommandContext(mockClient, *mockOutStream)
+		mockClient.OnGetVersionMatch(ctx, &admin.GetVersionRequest{}).Return(nil, fmt.Errorf("error"))
+		getControlePlaneVersion(ctx, cmdCtx)
+	})
+
 }
