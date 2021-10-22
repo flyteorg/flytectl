937759bbe456f420b1205e06ea243f6ee84db9fb
Dwaipayan <dwaipayan.sen@cimpress.com>
2021-10-22 13:18:52 +0530

    Add token in the filters (#1146)
    - Passing page as param is pending
    - Change in the config must also be pushed before using it
    
    Signed-off-by: Dwaipayan <dwaipayan.sen@cimpress.com>

--- pkg/filters/type.go
+++ pkg/filters/type.go
@@ -1,11 +1,13 @@
 package filters
 
 var (
-	DefaultLimit  int32 = 100
-	DefaultFilter       = Filters{
+	DefaultLimit  int32  = 100
+	DefaultToken  string = "1"
+	DefaultFilter        = Filters{
 		Limit:  DefaultLimit,
 		SortBy: "created_at",
 		Asc:    false,
+		Token:  DefaultToken,
 	}
 )
 
@@ -13,6 +15,7 @@ type Filters struct {
 	FieldSelector string `json:"fieldSelector" pflag:",Specifies the Field selector"`
 	SortBy        string `json:"sortBy" pflag:",Specifies which field to sort results "`
 	// TODO: Support paginated queries
-	Limit int32 `json:"limit" pflag:",Specifies the limit"`
-	Asc   bool  `json:"asc"  pflag:",Specifies the sorting order. By default flytectl sort result in descending order"`
+	Limit int32  `json:"limit" pflag:",Specifies the limit"`
+	Asc   bool   `json:"asc"  pflag:",Specifies the sorting order. By default flytectl sort result in descending order"`
+	Token string `protobuf:"bytes,4,opt,name=token,proto3" json:"token,omitempty"`
 }
