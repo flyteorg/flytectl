d51df1d9c640553fd6fcee2ea233fe2210c2cdd6
SmritiSatyanV <smriti@union.ai>
2021-12-09 11:33:17 +0530

    Creates to create

--- cmd/create/create_test.go
+++ cmd/create/create_test.go
@@ -39,5 +39,5 @@ func TestCreateCommand(t *testing.T) {
 	assert.Equal(t, cmdNouns[0].Short, executionShort)
 	assert.Equal(t, cmdNouns[1].Use, "project")
 	assert.Equal(t, cmdNouns[1].Aliases, []string{"projects"})
-	assert.Equal(t, cmdNouns[1].Short, "Creates project resources")
+	assert.Equal(t, cmdNouns[1].Short, "Create project resources")
 }
