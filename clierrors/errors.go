c1c6ad5834da379983bd84904abbb8e86ae12931
Samhita Alla <aallasamhita@gmail.com>
2022-03-15 12:45:13 +0530

    revert letter case in errors.go
    Signed-off-by: Samhita Alla <aallasamhita@gmail.com>

--- clierrors/errors.go
+++ clierrors/errors.go
@@ -1,24 +1,24 @@
 package clierrors
 
 var (
-	ErrInvalidStateUpdate = "Invalid state passed. Specify either activate or archive\n"
+	ErrInvalidStateUpdate = "invalid state passed. Specify either activate or archive\n"
 
-	ErrProjectNotPassed     = "Project id wasn't passed\n" // #nosec
-	ErrProjectNameNotPassed = "Project name is a required flag"
-	ErrFailedProjectUpdate  = "Project %v failed to update due to %v\n"
+	ErrProjectNotPassed     = "project id wasn't passed\n" // #nosec
+	ErrProjectNameNotPassed = "project name is a required flag"
+	ErrFailedProjectUpdate  = "project %v failed to update due to %v\n"
 
-	ErrLPNotPassed        = "Launch plan name wasn't passed\n"
-	ErrLPVersionNotPassed = "Launch plan version wasn't passed\n" //nolint
-	ErrFailedLPUpdate     = "Launch plan %v failed to update due to %v\n"
+	ErrLPNotPassed        = "launch plan name wasn't passed\n"
+	ErrLPVersionNotPassed = "launch plan version wasn't passed\n" //nolint
+	ErrFailedLPUpdate     = "launch plan %v failed to update due to %v\n"
 
-	ErrExecutionNotPassed    = "Execution name wasn't passed\n"
-	ErrFailedExecutionUpdate = "Execution %v failed to update due to %v\n"
+	ErrExecutionNotPassed    = "execution name wasn't passed\n"
+	ErrFailedExecutionUpdate = "execution %v failed to update due to %v\n"
 
-	ErrWorkflowNotPassed    = "Workflow name wasn't passed\n"
-	ErrFailedWorkflowUpdate = "Workflow %v failed to update to due to %v\n"
+	ErrWorkflowNotPassed    = "workflow name wasn't passed\n"
+	ErrFailedWorkflowUpdate = "workflow %v failed to update to due to %v\n"
 
-	ErrTaskNotPassed    = "Task name wasn't passed\n" // #nosec
-	ErrFailedTaskUpdate = "Task %v failed to update to due to %v\n"
+	ErrTaskNotPassed    = "task name wasn't passed\n" // #nosec
+	ErrFailedTaskUpdate = "task %v failed to update to due to %v\n"
 
-	ErrSandboxExists = "Sandbox already exists!\n"
+	ErrSandboxExists = "sandbox already exists!\n"
 )
