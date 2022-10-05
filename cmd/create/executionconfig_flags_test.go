3333ee00ea390846e5995752710b7446a9a0e48e
Nick Müller <nmueller@blackshark.ai>
2022-10-05 13:51:32 +0200

    Implemented skipCache flag for creating executions
    Signed-off-by: Nick Müller <nmueller@blackshark.ai>

--- cmd/create/executionconfig_flags_test.go
+++ cmd/create/executionconfig_flags_test.go
@@ -239,6 +239,20 @@ func TestExecutionConfig_SetFlags(t *testing.T) {
 			}
 		})
 	})
+	t.Run("Test_skipCache", func(t *testing.T) {
+
+		t.Run("Override", func(t *testing.T) {
+			testValue := "1"
+
+			cmdFlags.Set("skipCache", testValue)
+			if vBool, err := cmdFlags.GetBool("skipCache"); err == nil {
+				testDecodeJson_ExecutionConfig(t, fmt.Sprintf("%v", vBool), &actual.SkipCache)
+
+			} else {
+				assert.FailNow(t, err.Error())
+			}
+		})
+	})
 	t.Run("Test_workflow", func(t *testing.T) {
 
 		t.Run("Override", func(t *testing.T) {
