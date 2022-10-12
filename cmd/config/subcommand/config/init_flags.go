6a9f7c1b4b64ff69d3a277f5b10df9e9bee87375
Andrew Dye <andrewwdye@gmail.com>
2022-10-11 21:58:45 -0700

    Add console endpoint init arg and config section
    Signed-off-by: Andrew Dye <andrewwdye@gmail.com>

--- cmd/config/subcommand/config/init_flags.go
+++ cmd/config/subcommand/config/init_flags.go
@@ -7,8 +7,9 @@ var (
 	}
 )
 
-//Configs
+// Configs
 type Config struct {
+	Console  string `json:"console" pflag:",Endpoint of console, if different than flyte admin"`
 	Host     string `json:"host" pflag:",Endpoint of flyte admin"`
 	Insecure bool   `json:"insecure" pflag:",Enable insecure mode"`
 }
