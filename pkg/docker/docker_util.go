a0e2b81552057ccb9a380542d578f9e768a5a0d4
Jeev B <jeevb@users.noreply.github.com>
2023-08-31 16:48:24 -0700

    Add port 30004 for redis

--- pkg/docker/docker_util.go
+++ pkg/docker/docker_util.go
@@ -140,6 +140,7 @@ func GetDemoPorts() (map[nat.Port]struct{}, map[nat.Port][]nat.PortBinding, erro
 		"0.0.0.0:30001:30001", // Postgres Port
 		"0.0.0.0:30002:30002", // Minio API Port (use HTTP port for minio console)
 		"0.0.0.0:30003:30003", // Buildkit Port
+		"0.0.0.0:30004:30004", // Redis Port
 	})
 }
 
