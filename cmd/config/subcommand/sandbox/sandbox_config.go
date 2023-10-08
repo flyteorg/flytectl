cb6413de376bc457c59f01457f4ab4dd36e9b083
Future-Outlier <eric901201@gmail.com>
2023-10-08 20:41:11 +0800

    Update cmd/config/subcommand/sandbox/sandbox_config.go
    Co-authored-by: Kevin Su <pingsutw@gmail.com>
    Signed-off-by: Future-Outlier <eric901201@gmail.com>

--- cmd/config/subcommand/sandbox/sandbox_config.go
+++ cmd/config/subcommand/sandbox/sandbox_config.go
@@ -20,7 +20,7 @@ type Config struct {
 	Prerelease bool `json:"pre" pflag:",Optional. Pre release Version of flyte will be used for sandbox."`
 
 	// Agent Service
-	DisableAgent bool `json:"disable-agent" pflag:",Optional. Disable the agent feature."`
+	DisableAgent bool `json:"disable-agent" pflag:",Optional. Disable the agent service."`
 
 	// Optionally it is possible to pass in environment variables to sandbox container.
 	Env []string `json:"env" pflag:",Optional. Provide Env variable in key=value format which can be passed to sandbox container."`
