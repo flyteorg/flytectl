3dfcd947873ec95ac8c60a12115212808b4e9af5
Future Outlier <eric901201@gmai.com>
2023-11-16 22:58:08 +0800

    first commit
    Signed-off-by: Future Outlier <eric901201@gmai.com>

--- cmd/get/get.go
+++ cmd/get/get.go
@@ -1,6 +1,12 @@
 package get
 
 import (
+	"fmt"
+	"os"
+	"strings"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/fatih/color"
 	"github.com/flyteorg/flytectl/cmd/config/subcommand/clusterresourceattribute"
 	"github.com/flyteorg/flytectl/cmd/config/subcommand/execution"
 	"github.com/flyteorg/flytectl/cmd/config/subcommand/executionclusterlabel"
@@ -16,6 +22,87 @@ import (
 
 	"github.com/spf13/cobra"
 )
+
+type model struct {
+	choices  []string
+	cursor   int
+	selected string
+}
+
+var GetResourcesFuncs = map[string]cmdcore.CommandEntry{
+	"project": {CmdFunc: getProjectsFunc, Aliases: []string{"projects"}, ProjectDomainNotRequired: true,
+		Short: projectShort,
+		Long:  projectLong, PFlagProvider: project.DefaultConfig},
+	"task": {CmdFunc: getTaskFunc, Aliases: []string{"tasks"}, Short: taskShort,
+		Long: taskLong, PFlagProvider: task.DefaultConfig},
+	"workflow": {CmdFunc: getWorkflowFunc, Aliases: []string{"workflows"}, Short: workflowShort,
+		Long: workflowLong, PFlagProvider: workflow.DefaultConfig},
+	"launchplan": {CmdFunc: getLaunchPlanFunc, Aliases: []string{"launchplans"}, Short: launchPlanShort,
+		Long: launchPlanLong, PFlagProvider: launchplan.DefaultConfig},
+	"execution": {CmdFunc: getExecutionFunc, Aliases: []string{"executions"}, Short: executionShort,
+		Long: executionLong, PFlagProvider: execution.DefaultConfig},
+	"task-resource-attribute": {CmdFunc: getTaskResourceAttributes, Aliases: []string{"task-resource-attributes"},
+		Short: taskResourceAttributesShort,
+		Long:  taskResourceAttributesLong, PFlagProvider: taskresourceattribute.DefaultFetchConfig},
+	"cluster-resource-attribute": {CmdFunc: getClusterResourceAttributes, Aliases: []string{"cluster-resource-attributes"},
+		Short: clusterResourceAttributesShort,
+		Long:  clusterResourceAttributesLong, PFlagProvider: clusterresourceattribute.DefaultFetchConfig},
+	"execution-queue-attribute": {CmdFunc: getExecutionQueueAttributes, Aliases: []string{"execution-queue-attributes"},
+		Short: executionQueueAttributesShort,
+		Long:  executionQueueAttributesLong, PFlagProvider: executionqueueattribute.DefaultFetchConfig},
+	"execution-cluster-label": {CmdFunc: getExecutionClusterLabel, Aliases: []string{"execution-cluster-labels"},
+		Short: executionClusterLabelShort,
+		Long:  executionClusterLabelLong, PFlagProvider: executionclusterlabel.DefaultFetchConfig},
+	"plugin-override": {CmdFunc: getPluginOverridesFunc, Aliases: []string{"plugin-overrides"},
+		Short: pluginOverrideShort,
+		Long:  pluginOverrideLong, PFlagProvider: pluginoverride.DefaultFetchConfig},
+	"workflow-execution-config": {CmdFunc: getWorkflowExecutionConfigFunc, Aliases: []string{"workflow-execution-config"},
+		Short: workflowExecutionConfigShort,
+		Long:  workflowExecutionConfigLong, PFlagProvider: workflowexecutionconfig.DefaultFetchConfig, ProjectDomainNotRequired: true},
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "up":
+			if m.cursor > 0 {
+				m.cursor--
+			}
+		case "down":
+			if m.cursor < len(m.choices)-1 {
+				m.cursor++
+			}
+		case "enter":
+			m.selected = m.choices[m.cursor]
+			fmt.Println("You selected:", m.selected)
+			// if cmdFunc, exists := GetResourcesFuncs[m.selected]; exists {
+			// 	cmdFunc[m.selected]
+			// }
+
+			return m, tea.Quit
+		}
+	}
+	return m, nil
+}
+
+func (m model) View() string {
+	var s strings.Builder
+	blue := color.New(color.FgBlue)
+
+	for i, choice := range m.choices {
+		if i == m.cursor {
+			blue.Fprintf(&s, "> %s\n", choice)
+		} else {
+			fmt.Fprintf(&s, "  %s\n", choice)
+		}
+	}
+	return s.String()
+}
 
 // Long descriptions are whitespace sensitive when generating docs using sphinx.
 const (
@@ -34,41 +121,22 @@ func CreateGetCommand() *cobra.Command {
 		Use:   "get",
 		Short: getCmdShort,
 		Long:  getCmdLong,
+		Run: func(cmd *cobra.Command, args []string) {
+			p := tea.NewProgram(model{
+				choices: []string{"project", "task", "workflow", "launchplan",
+					"task-resource-attribute", "cluster-resource-attribute", "execution-queue-attribute",
+					"execution-cluster-label", "plugin-override", "workflow-execution-config"},
+			})
+			if err := p.Start(); err != nil {
+				fmt.Fprintf(os.Stderr, "Error occurred: %v\n", err)
+				os.Exit(1)
+			}
+		},
 	}
 
-	getResourcesFuncs := map[string]cmdcore.CommandEntry{
-		"project": {CmdFunc: getProjectsFunc, Aliases: []string{"projects"}, ProjectDomainNotRequired: true,
-			Short: projectShort,
-			Long:  projectLong, PFlagProvider: project.DefaultConfig},
-		"task": {CmdFunc: getTaskFunc, Aliases: []string{"tasks"}, Short: taskShort,
-			Long: taskLong, PFlagProvider: task.DefaultConfig},
-		"workflow": {CmdFunc: getWorkflowFunc, Aliases: []string{"workflows"}, Short: workflowShort,
-			Long: workflowLong, PFlagProvider: workflow.DefaultConfig},
-		"launchplan": {CmdFunc: getLaunchPlanFunc, Aliases: []string{"launchplans"}, Short: launchPlanShort,
-			Long: launchPlanLong, PFlagProvider: launchplan.DefaultConfig},
-		"execution": {CmdFunc: getExecutionFunc, Aliases: []string{"executions"}, Short: executionShort,
-			Long: executionLong, PFlagProvider: execution.DefaultConfig},
-		"task-resource-attribute": {CmdFunc: getTaskResourceAttributes, Aliases: []string{"task-resource-attributes"},
-			Short: taskResourceAttributesShort,
-			Long:  taskResourceAttributesLong, PFlagProvider: taskresourceattribute.DefaultFetchConfig},
-		"cluster-resource-attribute": {CmdFunc: getClusterResourceAttributes, Aliases: []string{"cluster-resource-attributes"},
-			Short: clusterResourceAttributesShort,
-			Long:  clusterResourceAttributesLong, PFlagProvider: clusterresourceattribute.DefaultFetchConfig},
-		"execution-queue-attribute": {CmdFunc: getExecutionQueueAttributes, Aliases: []string{"execution-queue-attributes"},
-			Short: executionQueueAttributesShort,
-			Long:  executionQueueAttributesLong, PFlagProvider: executionqueueattribute.DefaultFetchConfig},
-		"execution-cluster-label": {CmdFunc: getExecutionClusterLabel, Aliases: []string{"execution-cluster-labels"},
-			Short: executionClusterLabelShort,
-			Long:  executionClusterLabelLong, PFlagProvider: executionclusterlabel.DefaultFetchConfig},
-		"plugin-override": {CmdFunc: getPluginOverridesFunc, Aliases: []string{"plugin-overrides"},
-			Short: pluginOverrideShort,
-			Long:  pluginOverrideLong, PFlagProvider: pluginoverride.DefaultFetchConfig},
-		"workflow-execution-config": {CmdFunc: getWorkflowExecutionConfigFunc, Aliases: []string{"workflow-execution-config"},
-			Short: workflowExecutionConfigShort,
-			Long:  workflowExecutionConfigLong, PFlagProvider: workflowexecutionconfig.DefaultFetchConfig, ProjectDomainNotRequired: true},
-	}
+	fmt.Println("getCmd:", getCmd)
 
-	cmdcore.AddCommands(getCmd, getResourcesFuncs)
+	cmdcore.AddCommands(getCmd, GetResourcesFuncs)
 
 	return getCmd
 }
